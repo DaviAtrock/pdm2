9a2b0c708ed8732944a83593e546b380405b3bd9
DaviAtrock <daviatrock@gmail.com>
2023-08-29 21:21:10 -0300

    codigo aula02

---
+++ aula02/components/Box.js
@@ -0,0 +1,22 @@
+// hook useState
+import { useState } from 'react'
+
+import { View } from 'react-native'
+
+// componente
+const Box = (props) => {
+  const [state, setState] = useState("teste");
+  
+  // estilo inline
+  const boxStyle = {
+    // height: props.size,
+    // width: props.size,
+    flex: props.size,
+    backgroundColor: props.color
+  }
+  
+
+  return <View style = {boxStyle}></View>;
+};
+
+export default Box;
